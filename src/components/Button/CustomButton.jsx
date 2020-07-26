999d98a858b891e0b3aac07663d7ac811a84a7b8
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-27 01:27:32 +0530

    [ Darshan ] | CSS Improved

--- src/components/Button/CustomButton.jsx
+++ src/components/Button/CustomButton.jsx
@@ -3,8 +3,10 @@ import "./CustomButton.css";
 
 function CustomButton({ children, ...other }) {
   return (
-    <div className="btn btn-primary btnTab" {...other}>
-      {children}
+    <div className="customButtonWrapper">
+      <span className="btn btn-primary btnTab" {...other}>
+        {children}
+      </span>
     </div>
   );
 }
