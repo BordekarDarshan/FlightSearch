8473afd4a6b42e4becaf5c4da4b320ce5678cb26
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-27 19:52:38 +0530

    [ Darshan ] | Slider Added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import CustomButton from "../Button/CustomButton";
 import CustomDropdownOrigin from "../CustomDropdownOrigin/CustomDropdownOrigin";
@@ -10,6 +10,11 @@ function Sidebar({
   switchOneWay,
   switchReturn,
 }) {
+  const [slide, setSlide] = useState(0);
+  const handleChange = (event) => {
+    setSlide(event.target.value);
+  };
+
   return (
     <div className="sidebarWrapper">
       <div className="tabWrapper">
@@ -25,6 +30,18 @@ function Sidebar({
           title="Destination City"
           selectDestination={selectDestination}
         ></CustomDropdownDestination>
+      </div>
+      <div className="sliderWrapper">
+        <strong>Price</strong>
+        <input
+          type="range"
+          min="0"
+          max="10000"
+          value={slide}
+          onChange={handleChange}
+          step="1"
+        ></input>
+        <span>{slide}</span>
       </div>
       <div className="sideImage">
         <img alt="plane" src="https://i.ibb.co/0Y70z2y/blue-plane.png"></img>
