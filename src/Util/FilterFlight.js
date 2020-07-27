ffb047f44d0b5f839eb192ff54880e5d2348fb3f
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-27 12:23:25 +0530

    [ Darshan ] | Building Basic Logic

--- src/Util/FilterFlight.js
+++ src/Util/FilterFlight.js
@@ -1,16 +1,31 @@
 export function filterFlight(origin, destination, data) {
+  searchMultiAirline(origin, destination, data);
   if (origin === destination) {
     let originalData = sameFlightSelection(data);
     return originalData;
   } else {
-    let filteredData = data.filter(
-      (content) =>
-        content.origin === origin && content.destination === destination
-    );
+    let filteredData = filterFilghtByUser(origin, destination, data);
     return filteredData;
   }
 }
 
+const filterFilghtByUser = (origin, destination, data) => {
+  let filteredData = data.filter(
+    (content) =>
+      content.origin === origin && content.destination === destination
+  );
+  return filteredData;
+};
+
 const sameFlightSelection = (data) => {
   return data;
+};
+
+// origin's destination === destination's origin
+
+const searchMultiAirline = (origin, destination, data) => {
+  let originData = data.filter((data) => data.origin === origin);
+  let destinationData = data.filter((data) => data.destination === destination);
+  let concatedData = [...originData, ...destinationData];
+  return concatedData;
 };
