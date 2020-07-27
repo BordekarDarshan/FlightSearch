dc48fa1f4614480870fb3c4baeda720c641997c4
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-27 13:36:04 +0530

    [ Darshan ] | Added Functionality

--- src/components/BookFlight/BookFlight.jsx
+++ src/components/BookFlight/BookFlight.jsx
@@ -5,11 +5,18 @@ import "./BookFlight.css";
 
 function BookFlight({ data, origin, destination }) {
   let filteredData = filterFlight(origin, destination, data);
+
   return (
     <div className="bookFlightWrapper">
       <div className="selectedFlight">
-        <span className="flightName">Name</span>
-        <span className="flightResult">Result</span>
+        {origin && destination && (
+          <span className="flightName">
+            {origin} To {destination}
+          </span>
+        )}
+        <span className="flightResult">
+          <strong>{filteredData.length}</strong> Flights Found
+        </span>
       </div>
       <div className="flightScheduleWrapper">
         <FlightCard data={filteredData}></FlightCard>
