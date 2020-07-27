f5f4f8923c90fc73ad3e0772d4f55ecf4d9e7f5c
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-28 00:43:02 +0530

    [ Darshan ] | Accordian Added

--- src/components/MultilineFlightCard/MultilineFlightCard.jsx
+++ src/components/MultilineFlightCard/MultilineFlightCard.jsx
@@ -1,29 +1,59 @@
 import React from "react";
 import "./MultilineFlightCard.css";
 import CustomButton from "../Button/CustomButton";
+import { Accordion, Button } from "react-bootstrap";
 
 function MultilineFlightCard({ multiline }) {
   return (
-    <div className="card mutilineFlightCardWrapper">
-      <div className="mutilineFlightId">
-        <strong>{multiline.name}</strong>
-        <CustomButton>Details</CustomButton>
-      </div>
-      <div className="mutilineFlightDuration">
-        <div className="mutilineDeparture">
-          <strong>{multiline.departureTime}</strong>
-          <strong>{multiline.origin}</strong>
+    <Accordion>
+      <div className="card mutilineFlightCardWrapper">
+        <div className="mutilineFlightId">
+          <strong>{multiline.name}</strong>
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            Details
+          </Accordion.Toggle>
         </div>
-        <div className="mutilineArrival">
-          <strong>{multiline.arrivalTime}</strong>
-          <strong>{multiline.destination}</strong>
+        <div className="mutilineFlightDuration">
+          <div className="mutilineDeparture">
+            <strong>{multiline.departureTime}</strong>
+            <strong>{multiline.origin}</strong>
+          </div>
+          <div className="mutilineArrival">
+            <strong>{multiline.arrivalTime}</strong>
+            <strong>{multiline.destination}</strong>
+          </div>
+        </div>
+        <div className="mutilinePayment">
+          <strong>₹ {multiline.price}</strong>
+          <CustomButton>Book</CustomButton>
         </div>
       </div>
-      <div className="mutilinePayment">
-        <strong>₹ {multiline.price}</strong>
-        <CustomButton>Book</CustomButton>
-      </div>
-    </div>
+      <Accordion.Collapse eventKey="0">
+        <>
+          {multiline.multiple.map((data) => (
+            <div className="card mutilineFlightCardWrapper">
+              <div className="mutilineFlightId">
+                <strong>{data.name}</strong>
+              </div>
+              <div className="mutilineFlightDuration">
+                <div className="mutilineDeparture">
+                  <strong>{data.departureTime}</strong>
+                  <strong>{data.origin}</strong>
+                </div>
+                <div className="mutilineArrival">
+                  <strong>{data.arrivalTime}</strong>
+                  <strong>{data.destination}</strong>
+                </div>
+              </div>
+              <div className="mutilinePayment">
+                <strong>₹ {data.price}</strong>
+                <CustomButton>Book</CustomButton>
+              </div>
+            </div>
+          ))}
+        </>
+      </Accordion.Collapse>
+    </Accordion>
   );
 }
 
