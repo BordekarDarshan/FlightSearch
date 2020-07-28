8ffdac8b8c914cd0479f08020790dacc4ee84307
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-28 11:22:42 +0530

    [ Darshan ] | Conditional Rendering

--- src/Util/searchMultiAirline.js
+++ src/Util/searchMultiAirline.js
@@ -1,23 +1,27 @@
 export const searchMultiAirline = (origin, destination, data) => {
-  let originData = data.filter((data) => data.origin === origin);
-  let destinationData = data.filter((data) => data.destination === destination);
-
-  for (const origin of originData) {
-    for (const destination of destinationData) {
-      if (origin.destination === destination.origin) {
-        const finalPrice = calculatePrice(origin, destination);
-        let refinedContent = {
-          id: 1,
-          multiple: [origin, destination],
-          name: "Multiple",
-          TotalDuration: "",
-          departureTime: origin.departureTime,
-          arrivalTime: destination.arrivalTime,
-          price: finalPrice,
-          origin: origin.origin,
-          destination: destination.destination,
-        };
-        return refinedContent;
+  if (origin === destination) {
+    return null;
+  } else {
+    let originData = data.filter((data) => data.origin === origin);
+    let destinationData = data.filter(
+      (data) => data.destination === destination
+    );
+    for (const origin of originData) {
+      for (const destination of destinationData) {
+        if (origin.destination === destination.origin) {
+          let refinedContent = {
+            id: 1,
+            multiple: [origin, destination],
+            name: "Multiple",
+            TotalDuration: "",
+            departureTime: origin.departureTime,
+            arrivalTime: destination.arrivalTime,
+            price: calculatePrice(origin, destination),
+            origin: origin.origin,
+            destination: destination.destination,
+          };
+          return refinedContent;
+        }
       }
     }
   }
